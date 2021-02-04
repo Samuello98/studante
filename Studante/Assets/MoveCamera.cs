a25507c0f7709c85be2524f29247e58fc409e85d
Samuello <samuele.mannala@gmail.com>
2021-02-04 12:18:02 +0100

    New First Person Controller

---
+++ Studante/Assets/MoveCamera.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+
+    public Transform player;
+
+    void Update()
+    {
+
+        transform.position = player.transform.position;
+
+    }
+}
