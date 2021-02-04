0f476223cee8201c1e30140bd4003190e8675be2
Samuello <samuele.mannala@gmail.com>
2021-02-04 12:18:02 +0100

    New First Person Controller

---
+++ Studante/Assets/PhysicsObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour
+{
+    public float waitOnPickUp = 0.2f;
+    public float breakForce = 25f;
+    [HideInInspector] public bool pickedUp = false;
+    [HideInInspector] public PlayerInteractions playerInteractions;
+
+    public void OnCollisionEnter(Collision collision)
+    {
+        if(pickedUp)
+        {
+            if(collision.relativeVelocity.magnitude > breakForce)
+            {
+                playerInteractions.BreakConnection();
+            }
+        }
+    }
+
+
+    public IEnumerator PickUp()
+    {
+        yield return new WaitForSecondsRealtime(waitOnPickUp);
+    }
+}
---
+++ Studante/Assets/PlayerInteractions.cs
@@ -0,0 +1,116 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteractions : MonoBehaviour
+{
+    [Header("InteractableInfo")]
+    public float sphereCastRadius = 0.5f;
+    public int interactableLayerIndex;
+    public Vector3 raycastPos;
+    public GameObject lookObject;
+    private PhysicsObject physicsObject;
+    public Camera mainCamera;
+
+    [Header("Pickup")]
+    [SerializeField] private Transform pickupParent;
+    public GameObject currentlyPickedUpObject;
+    private Rigidbody pickupRB;
+
+    [Header("ObjectToFollow")]
+    [SerializeField] private float minSpeed = 0;    
+    [SerializeField] private float maxSpeed = 300f;
+    [SerializeField] private float maxDistance = 10f;
+    private float currentSpeed = 0f;
+    private float currentDist = 0f;
+
+    [Header("Rotation")]
+    public float rotationSpeed = 100f;
+    Quaternion lookRot;
+
+    void Start()
+    {
+        mainCamera = Camera.main; 
+    }
+
+
+    
+    //visualizzazione del punto che stiamo seguendo
+    private void OndrawGizmos()
+    {
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawSphere(pickupParent.position, 0.5f);
+
+    }
+
+    // Interactable Object detections and distance check
+    void Update()
+    {
+        Debug.Log(mainCamera);
+        raycastPos = mainCamera.ScreenToWorldPoint(new Vector3(Screen.width / 2, Screen.height / 2, 0));
+        RaycastHit hit;
+        if(Physics.SphereCast(raycastPos, sphereCastRadius, mainCamera.transform.forward, out hit, maxDistance, 1<< interactableLayerIndex))
+        {
+            lookObject = hit.collider.transform.root.gameObject;
+        }
+        else
+        {
+            lookObject = null;
+        }
+
+        if(Input.GetButtonDown("Fire2"))
+        {
+            if(currentlyPickedUpObject==null)
+            {
+                if (lookObject != null)
+                {
+                    PickUpObject();
+                }
+            }
+            else
+            {
+                BreakConnection();
+            }
+        }
+        if (currentlyPickedUpObject != null && currentDist > maxDistance) BreakConnection();
+
+    }
+
+    private void FixedUpdate()
+    {
+        if(currentlyPickedUpObject != null)
+        {
+            currentDist = Vector3.Distance(pickupParent.position, pickupRB.position);
+            currentSpeed = Mathf.SmoothStep(minSpeed, maxSpeed, currentDist / maxDistance);
+            currentSpeed *= Time.fixedDeltaTime;
+            Vector3 direction = pickupParent.position - pickupRB.position;
+            //rotation
+            lookRot = Quaternion.LookRotation(mainCamera.transform.position - pickupRB.position);
+            lookRot = Quaternion.Slerp(mainCamera.transform.rotation, lookRot, rotationSpeed * Time.fixedDeltaTime);
+            pickupRB.MoveRotation(lookRot);
+        }
+        
+    }
+
+    public void BreakConnection()
+    {
+        pickupRB.constraints = RigidbodyConstraints.None;
+        currentlyPickedUpObject = null;
+        physicsObject.pickedUp = false;
+        currentDist = 0;
+    }
+    
+    public void PickUpObject()
+    {
+        physicsObject = lookObject.GetComponentInChildren<PhysicsObject>();
+        currentlyPickedUpObject = lookObject;
+        pickupRB = currentlyPickedUpObject.GetComponent<Rigidbody>();
+        pickupRB.constraints = RigidbodyConstraints.FreezeRotation;
+        physicsObject.playerInteractions = this;
+        StartCoroutine(physicsObject.PickUp());
+
+    }
+
+
+
+}
