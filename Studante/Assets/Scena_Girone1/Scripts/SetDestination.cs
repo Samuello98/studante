038ec64b3b055ca65ef591f5ac3714adf43057ae
Gabriele Campanella <gabriele.campanella99@gmail.com>
2021-02-11 17:00:02 +0100

    Tornado No physics

---
+++ Studante/Assets/Scena_Girone1/Scripts/SetDestination.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class SetDestination : MonoBehaviour
+{
+    public List<Transform> _waypoints;
+    public NavMeshAgent _navMeshAgent;
+    public int _currentWayPointIndex = 0;
+
+    void Update()
+    {
+        SetWayPointsDestination();
+    }
+
+    void SetWayPointsDestination()
+    {
+        _currentWayPointIndex = (_currentWayPointIndex + 1) % _waypoints.Count;
+        Vector3 nextWayPointPos = _waypoints[_currentWayPointIndex].position;
+        _navMeshAgent.SetDestination(new Vector3(nextWayPointPos.x, transform.position.y, nextWayPointPos.z));
+    }
+
+}
+
+
