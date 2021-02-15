89501e4aadb217225a9431dcfdd10a8a79540035
Gabriele Campanella <gabriele.campanella99@gmail.com>
2021-02-15 12:06:32 +0100

    Girone P&F Implementazione trigger dialogo

---
+++ Studante/Assets/TriggerStop.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerStop : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private bool triggered = false;
+    public Dialogue dialogue;
+    public Animator animator;
+
+    // Update is called once per frame
+    private void OnTriggerEnter(Collider other)
+    {
+        if (triggered == false)
+        {
+            animator.SetBool("rotating", false);
+            triggered = true;
+            TriggerDialogue();
+        }
+    }
+    public void TriggerDialogue()
+    {
+        FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+
+    }
+}
