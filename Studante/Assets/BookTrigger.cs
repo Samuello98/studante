b91e52a7c99537114f86b8ed7794da791133dbba
Gabriele Campanella <gabriele.campanella99@gmail.com>
2021-02-15 18:36:06 +0100

    Fine Implementazione Dialoghi

---
+++ Studante/Assets/BookTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class BookTrigger : MonoBehaviour
+{
+    private bool triggered = false;
+    public Dialogue dialogue;
+    
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.name == "libro_aperto")
+        {
+            FindObjectOfType<DialogueManagerLibro>().StartDialogue(dialogue);
+        }
+    }
+}
